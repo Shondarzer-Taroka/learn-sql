9e458760b64371b6bf2069bdf2ee99cd182b308f
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-11-18 18:51:29 +0600

    set default in existing table rows

---
+++ defult in new table.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE lovers (
+-- lover_name VARCHAR(21),
+-- lover_money DECIMAL(24,2) DEFAULT 10.00,
+-- lover_father_name VARCHAR(13),
+-- lover_mother_name VARCHAR(13)
+-- )
+
+INSERT INTO lovers(lover_name,lover_father_name,lover_mother_name)
+VALUES('Bhoko','Gloiksd','werksjh');
+
+select * from lovers
+
+
---
+++ set default in existing table rows.sql
@@ -0,0 +1,9 @@
+-- ALTER TABLE products
+-- ALTER price SET DEFAULT 0
+
+-- INSERT INTO products
+-- VALUES ()
+INSERT INTO products(product_id,product_name)
+VALUES (5,"GOKA"),
+(6,"TOIK");
+SELECT * FROM products
