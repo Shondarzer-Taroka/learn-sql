f01dfc2da33057c2d75715caa30fd2f76df77ee7
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-11-18 14:42:29 +0600

    delete table and current date time

---
+++ current time date datetime create and insert values.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE test (
+-- my_date DATE,
+-- my_time TIME,
+-- my_datetime DATETIME
+-- )
+
+-- INSERT INTO test
+-- VALUES (CURRENT_DATE(),CURRENT_TIME(),NOW());
+
+
+INSERT INTO test
+VALUES (CURRENT_DATE(),null,null);
+SELECT * FROM test
