6c29a7674939236c205bb81dd5e90a98c2fa856f
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-11-24 22:38:17 +0600

    practice table;

--- specific cloumn data.sql
+++ specific cloumn data.sql
@@ -1,3 +1,24 @@
-SELECT *
-FROM employees
-WHERE hire_date IS NULL;
+
+-- create table ratable(
+-- personName varchar(12),
+-- email varchar(13)
+-- );
+
+-- alter table ratable
+-- change res mobileNumber varchar(12); 
+-- rename column mobileNumber to res;
+
+-- alter table ratable
+-- modify column rata_id int auto_increment primary key  first ;
+-- ALTER TABLE ratable
+-- MODIFY COLUMN rata_id INT  AUTO_INCREMENT  FIRST;
+
+delete from ratable
+where rata_id=1;
+
+-- insert into ratable
+-- (personName,email,res)
+-- values('jkljlk','kljkl','jkoi');
+
+select * from ratable;
+
